28e7445174e02aa0f0b0f38a0200a1b33b72e6a6
seandominicyap-cpu <sean.dominic.yap@student.ateneo.edu>
2026-03-03 17:23:23 +0800

    Created admin panel, added templates for views, fixed bugs

--- commongrounds/bookclub/admin.py
+++ commongrounds/bookclub/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Book, Genre
 
 # Register your models here.
+
+
+class BookAdmin(admin.ModelAdmin):
+    model = Book
+
+
+class GenreAdmin(admin.ModelAdmin):
+    model = Genre
+
+
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Book, BookAdmin)
