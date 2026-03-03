ffae3fda625aba28e8383820cb272e1deaf25d8b
Shizuka <gcabbage2391@gmail.com>
2026-03-03 23:29:00 +0800

    Created models for commissions app

--- commongrounds/commissions/models.py
+++ commongrounds/commissions/models.py
@@ -4,3 +4,10 @@ from django.db import models
 class CommissionType(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
+
+class Commission(modesl.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    people_required = models.IntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
