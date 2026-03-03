459f323fd74aad4696a16fb6a7b63ccd9c021ef4
Shizuka <gcabbage2391@gmail.com>
2026-03-03 22:44:08 +0800

    Added views for commissions website

--- commongrounds/commissions/views.py
+++ commongrounds/commissions/views.py
@@ -1,3 +1,14 @@
+"""Views for commissions project"""
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
 # Create your views here.
+
+
+class CommissionListView(TemplateView):
+    """View to list commissions"""
+    template_name = "commissions/commission_list.html"
+
+
+class CommissionDetailView(TemplateView):
+    """View to show commissions details"""
+    template_name = "commissions/comission_detail.html"
