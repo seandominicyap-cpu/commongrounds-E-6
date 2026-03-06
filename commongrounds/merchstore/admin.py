b5303a30c9ba0547a3743614aedd89320f05be04
tatine <tatineeee@gmail.com>
2026-03-06 15:12:01 +0800

    created models for product and product type, detail view and list view, admin panel, and urls

--- commongrounds/merchstore/admin.py
+++ commongrounds/merchstore/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import ProductType, Product
 
-# Register your models here.
+
+class ProductInline(admin.TabularInline):
+    model = Product
+
+
+class ProductTypeAdmin(admin.ModelAdmin):
+    model = ProductType
+    inlines = [ProductInline]
+
+
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+    list_display = ("name", "product_type", "price")
+    list_filter = ("product_type",)
+
+
+admin.site.register(ProductType, ProductTypeAdmin)
+admin.site.register(Product, ProductAdmin)
--- commongrounds/merchstore/views.py
+++ commongrounds/merchstore/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Product
 
-# Create your views here.
+
+class ProductListView(ListView):
+    model = Product
+    template_name = "merchstore/item_list.html"
+    context_object_name = "items"
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "merchstore/item_detail.html"
+    context_object_name = "item"
