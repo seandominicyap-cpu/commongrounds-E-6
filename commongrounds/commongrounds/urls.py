aa8498aec44ff8680a336d05c7dbfff12d5790b0
Shizuka <gcabbage2391@gmail.com>
2026-03-06 22:35:43 +0800

    Changed single quotes to double quotes in urls

--- commongrounds/commongrounds/urls.py
+++ commongrounds/commongrounds/urls.py
@@ -6,23 +6,23 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a URL to urlpatterns:  path("", views.home, name="home")
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path("", Home.as_view(), name="home")
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path("blog/", include("blog.urls"))
 """
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('bookclub/', include('bookclub.urls', namespace="bookclub")),
-    path('localevents/', include('localevents.urls')),
-    path('diyprojects/', include('diyprojects.urls'))
-    path('commissions/', include(('commissions.urls', 'commissions'), namespace="commissions"))
-    path('merchandise/', include('merchandise.urls'))
+    path("admin/", admin.site.urls),
+    path("bookclub/", include("bookclub.urls", namespace="bookclub")),
+    path("localevents/", include("localevents.urls")),
+    path("diyprojects/", include("diyprojects.urls"))
+    path("commissions/", include(("commissions.urls", "commissions"), namespace="commissions"))
+    path("merchstore/", include("merchstore.urls")),
 ]
 
