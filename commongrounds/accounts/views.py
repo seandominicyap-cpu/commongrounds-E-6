e6fcde0aaf391cd75b1c04279945506fc9404c40
Shizuka <gcabbage2391@gmail.com>
2026-04-30 06:22:12 +0800

    Created profile update form template

--- commongrounds/accounts/views.py
+++ commongrounds/accounts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import UpdateView
+from .models import Profile
 
 # Create your views here.
+class PostUpdateView(UpdateView):
+    model = Profile
+    fields = ['display_name']
+    template_name = 'profile_form.html'
