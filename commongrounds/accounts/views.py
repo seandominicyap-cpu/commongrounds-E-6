87fb8c15b1abb1965ff023200b217abf5592755f
Shizuka <gcabbage2391@gmail.com>
2026-05-01 01:49:07 +0800

    Modified profile update view to work

--- commongrounds/accounts/views.py
+++ commongrounds/accounts/views.py
@@ -7,3 +7,9 @@ class AccountUpdateView(UpdateView):
     model = Profile
     fields = ['display_name']
     template_name = 'profile_form.html'
+
+    def get_success_url(self):
+        return f"/accounts/{self.object.user.username}/"
+
+    def get_object(self):
+        return Profile.objects.get(user__username=self.kwargs['username'])
