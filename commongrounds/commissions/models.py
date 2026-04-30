b36e6ec14ecd10e6f9444e412e5a6801c0bbc6a5
Shizuka <gcabbage2391@gmail.com>
2026-05-01 04:19:11 +0800

    Updated Commissions model to include maker and status

--- commongrounds/commissions/models.py
+++ commongrounds/commissions/models.py
@@ -1,6 +1,7 @@
 """Models of commmissions app."""
 
 from django.db import models
+from accounts.models import Profile
 # Create your models here.
 
 
@@ -18,6 +19,10 @@ class CommissionType(models.Model):
 
 class Commission(models.Model):
     """Model that represents commissions."""
+    STATUS_CHOICES = [
+        ('OPEN', 'Open'),
+        ('FULL', 'Full'),
+    ]
 
     title = models.CharField(max_length=255)
     commission_type = models.ForeignKey(
@@ -29,6 +34,15 @@ class Commission(models.Model):
     )
     description = models.TextField()
     people_required = models.IntegerField()
+    maker = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+    )
+    status = models.CharField(
+        max_length = 20,
+        choices = STATUS_CHOICES,
+        default = 'OPEN'
+    )
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
