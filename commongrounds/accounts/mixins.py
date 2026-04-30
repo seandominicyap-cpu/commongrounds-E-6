9e84182bedadcbdf5381a05d4b4700e765dd5085
Shizuka <gcabbage2391@gmail.com>
2026-05-01 02:02:47 +0800

    Created RoleRequiredMixin to authenticate roles

---
+++ commongrounds/accounts/mixins.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.core.exceptions import PermissionDenied
+
+class RoleRequiredMixin(UserPassesTestMixin):
+    allowed_roles = []
+
+    def test_func(self):
+        return self.request.user.profile.roles.filter(
+            name__in=self.allowed_roles
+        ).exists()
+
+    def handle_no_permission(self):
+        if not self.request.user.is_authenticated:
+            return super().handle_no_permission()
+        raise PermissionDenied
--- commongrounds/accounts/models.py
+++ commongrounds/accounts/models.py
@@ -3,8 +3,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Role(models.Model):
     name = models.CharField(max_length=50)
-    
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    roles = models.ManyToManyField(Role)
     display_name = models.CharField(max_length=63)
     email_address = models.EmailField()
