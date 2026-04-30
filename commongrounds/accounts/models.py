b7b31d08cedb6da87cef8b57c24b19c0fe448e97
Shizuka <gcabbage2391@gmail.com>
2026-05-01 01:56:25 +0800

    Created roles model

--- commongrounds/accounts/models.py
+++ commongrounds/accounts/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+class Role(models.Model):
+    name = models.CharField(max_length=50)
+    
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     display_name = models.CharField(max_length=63)
