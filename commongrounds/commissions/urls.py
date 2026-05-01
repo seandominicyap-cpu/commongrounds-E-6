5f7d390abcb9cbc9b1d891b38f087da512f2fb04
Shizuka <gcabbage2391@gmail.com>
2026-05-01 23:12:13 +0800

    Linked CommissionLinkView with CommissionCreateView

--- commongrounds/commissions/urls.py
+++ commongrounds/commissions/urls.py
@@ -6,6 +6,6 @@ from .views import CommissionDetailView, CommissionListView, CommissionCreateVie
 urlpatterns = [
     path("requests/list", CommissionListView.as_view(), name="commission_detail"),
     path("request/<int:pk>", CommissionDetailView.as_view(), name="commission_list"),
-    path("request/add", CommissionCreateView.as_view(), name="commission_list"),
-    path("request/<int:pk>/edit", CommissionUpdateView.as_view(), name="commission_list"),
+    path("request/add", CommissionCreateView.as_view(), name="commission_create"),
+    path("request/<int:pk>/edit", CommissionUpdateView.as_view(), name="commission_update"),
 ]
