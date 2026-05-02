a10857598edd44b744899051b0058948d824d5e2
tatine <tatineeee@gmail.com>
2026-05-02 20:58:53 +0800

    added profile and account usage for merchstore, added admin and models for accounts

--- commongrounds/accounts/models.py
+++ commongrounds/accounts/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+
 # Create your models here.
 class Role(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     roles = models.ManyToManyField(Role)
     display_name = models.CharField(max_length=63)
     email_address = models.EmailField()
+
+    def __str__(self):
+        if self.display_name:
+            return self.display_name
+        return self.user.username
