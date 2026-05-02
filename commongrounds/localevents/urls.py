1b7aa91ce1b011775f2a50a88ee5ac3a3d4949b3
HDT-8398 <hansdanieltan@gmail.com>
2026-05-02 09:55:46 +0800

    Added URLs

--- commongrounds/localevents/urls.py
+++ commongrounds/localevents/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path("events/", views.event_list, name="event_list"),
     path("event/<int:event_id>/", views.event_detail, name="event_detail"),
+    path("event/add/", views.event_create, name="event_create"), 
+    path("event/<int:event_id>/edit/", views.event_update, name="event_update" ),
+    path("event/<int:event_id>/signup/", views.event_signup, name="event_signup"),
 ]
