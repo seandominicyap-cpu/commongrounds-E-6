294eba8bef3d3826f5b2b97aa35a630cdc09c730
seandominicyap-cpu <sean.dominic.yap@student.ateneo.edu>
2026-05-03 11:00:11 +0800

    Modified admin panel

--- commongrounds/bookclub/admin.py
+++ commongrounds/bookclub/admin.py
@@ -1,15 +1,30 @@
 from django.contrib import admin
-from .models import Book, Genre
+from .models import Book, Genre, BookReview, Bookmark, Borrow
 # Register your models here.
-
-
-class BookAdmin(admin.ModelAdmin):
-    model = Book
 
 
 class GenreAdmin(admin.ModelAdmin):
     model = Genre
 
 
+class BookAdmin(admin.ModelAdmin):
+    model = Book
+
+
+class BookReviewAdmin(admin.ModelAdmin):
+    model = BookReview
+
+
+class BookmarkAdmin(admin.ModelAdmin):
+    model = Bookmark
+
+
+class BorrowAdmin(admin.ModelAdmin):
+    model = Borrow
+
+
 admin.site.register(Genre, GenreAdmin)
 admin.site.register(Book, BookAdmin)
+admin.site.register(BookReview, BookReviewAdmin)
+admin.site.register(Bookmark, BookmarkAdmin)
+admin.site.register(Borrow, BorrowAdmin)
