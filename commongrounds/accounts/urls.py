526c2fa640f16eafcde0455d36705960f4109531
Shizuka <gcabbage2391@gmail.com>
2026-05-06 04:00:40 +0800

    Added basic registration functionality

--- commongrounds/accounts/urls.py
+++ commongrounds/accounts/urls.py
@@ -1,10 +1,10 @@
 """URL configuration for commissions app."""
 
 from django.urls import path, include
-from .views import AccountUpdateView
+from .views import AccountUpdateView, RegisterView
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
-
+    path('register/', RegisterView.as_view(), name='register'),
     path('<str:username>/', AccountUpdateView.as_view(), name='accounts_update'),
 ]
--- commongrounds/accounts/views.py
+++ commongrounds/accounts/views.py
@@ -1,10 +1,9 @@
 from django.urls import reverse_lazy
-from django.views.generic import UpdateView
+from django.views.generic import UpdateView, TemplateView
 from .models import Profile
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .mixins import RoleRequiredMixin
-
-
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect, get_object_or_404
 # Create your views here.
 class AccountUpdateView(LoginRequiredMixin, UpdateView):
     model = Profile
@@ -15,4 +14,24 @@ class AccountUpdateView(LoginRequiredMixin, UpdateView):
         return f"/accounts/{self.object.user.username}/"
 
     def get_object(self):
-        return Profile.objects.get(user__username=self.kwargs["username"])
+        return get_object_or_404(Profile, user__username=self.kwargs["username"])
+
+
+class RegisterView(TemplateView):
+    template_name = "register.html"
+    def get(self, request):
+        return render(request, "register.html", {
+            "user_form": UserCreationForm(),
+        })
+
+    def post(self, request):
+        user_form = UserCreationForm(request.POST)
+
+        if user_form.is_valid():
+            user_form.save()
+
+            return redirect("login")
+
+        return render(request, "register.html", {
+            "user_form": user_form,
+        })
