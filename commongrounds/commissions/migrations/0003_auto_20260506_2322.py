fac132230d610186884f5d7e57d32b691c4a6b67
Shizuka <gcabbage2391@gmail.com>
2026-05-06 23:28:45 +0800

    Added proper migrations to commissions

---
+++ commongrounds/commissions/migrations/0003_auto_20260506_2322.py
@@ -0,0 +1,32 @@
+from django.db import migrations
+
+def create_job_statuses(apps, schema_editor):
+    ApplicationStatus = apps.get_model('commissions', 'ApplicationStatus')
+
+    statuses = [
+        ("PENDING", 1),
+        ("ACCEPTED", 2),
+        ("REJECTED", 3),
+    ]
+
+    for name, order in statuses:
+        ApplicationStatus.objects.update_or_create(
+            name=name,
+            defaults={"order": order}
+        )
+
+def delete_job_statuses(apps, schema_editor):
+    ApplicationStatus = apps.get_model('commissions', 'ApplicationStatus')
+    ApplicationStatus.objects.filter(
+        name__in=["PENDING", "ACCEPTED", "REJECTED"]
+    ).delete()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('commissions', '0002_auto_20260506_2318'),
+    ]
+
+    operations = [
+        migrations.RunPython(create_job_statuses, delete_job_statuses),
+    ]
