8c2aeafa1829e09dfce29a39503e379f37076248
Shizuka <gcabbage2391@gmail.com>
2026-05-07 00:19:11 +0800

    Made images visible in development

--- commongrounds/commongrounds/urls.py
+++ commongrounds/commongrounds/urls.py
@@ -18,6 +18,8 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import HomePageView
 
 urlpatterns = [
@@ -30,3 +32,7 @@ urlpatterns = [
     path("merchstore/", include("merchstore.urls")),
     path("", HomePageView.as_view(), name="home"),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
